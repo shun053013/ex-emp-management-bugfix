37023fac427681b64271811d2c8f0fb82a848052
shun053013 <shun053013@gmail.com>
2019-11-14 16:06:21 +0900

    初級1-3終わりました。

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -77,7 +77,7 @@ public class AdministratorController {
 		// フォームからドメインにプロパティ値をコピー
 		BeanUtils.copyProperties(form, administrator);
 		administratorService.insert(administrator);
-		return "redirect:/administrator/login";
+		return "redirect:/";
 	}
 
 	/////////////////////////////////////////////////////

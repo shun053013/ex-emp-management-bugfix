f06620d65f33867eac50ad5e86bbdf1006ccc2e9
shun053013 <shun053013@gmail.com>
2019-11-15 15:16:05 +0900

    (2-1)中級 ログイン者名表示

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -88,7 +88,7 @@ public class AdministratorController {
 		// フォームからドメインにプロパティ値をコピー
 		BeanUtils.copyProperties(form, administrator);
 		administratorService.insert(administrator);
-		   return "/";
+		   return "administrator/login";
 		
 			
 		}
